251dd088f041cdc3c5013efe65e67d9031c946ac
kimplausible <andyk1278@gmail.com>
2018-06-19 00:26:36 -0700

    edit base.html for login link. created Login form and routes.

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,19 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'supadood'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'What a splendid day it is!'
+        },
+        {
+            'author': {'username': 'droopy'},
+            'body': 'The Avengers series is so well made imo.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
